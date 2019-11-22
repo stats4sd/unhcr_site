53d80a21347b183cf969b64c1e7af4b47fee54cf
David Mills <d.e.mills@stats4sd.org>
2019-11-22 16:01:26 +0000

    adding dotenv in R to read from .env file instead of hard coding DB config

--- R/dbConfig.R
+++ R/dbConfig.R
@@ -1,12 +1,12 @@
 library(DBI)
-library(RMySQL)
+library(dotenv)
 #Include databasename, user and password 
 
 con <- dbConnect(RMySQL::MySQL(),
-                 dbname = "unhcr",
-                 host ='127.0.0.1', 
-                 port = 3306,
-                 user = 'root',
-                 password = "alemonflavour")
-
+                 dbname = Sys.getenv('DB_DATABASE'),
+                 host = Sys.getenv('DB_HOST'), 
+                 port = as.numeric(Sys.getenv('DB_PORT')),
+                 user = Sys.getenv('DB_USERNAME'),
+                 password = Sys.getenv('DB_PASSWORD')
+)
 
