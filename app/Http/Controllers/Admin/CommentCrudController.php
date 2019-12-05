a72034076d48d66c81a586dbcdf170c90aa13b1a
David Mills <d.e.mills@stats4sd.org>
2019-12-05 15:48:29 +0000

    adding tags to comments

--- app/Http/Controllers/Admin/CommentCrudController.php
+++ app/Http/Controllers/Admin/CommentCrudController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Requests\CommentRequest;
+use App\Models\Comment;
 use Backpack\CRUD\app\Http\Controllers\CrudController;
 use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
+use Spatie\Tags\Tag;
 
 /**
  * Class CommentCrudController
@@ -14,8 +16,8 @@ use Backpack\CRUD\app\Library\CrudPanel\CrudPanelFacade as CRUD;
 class CommentCrudController extends CrudController
 {
     use \Backpack\CRUD\app\Http\Controllers\Operations\ListOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation;
-    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation;
+    use \Backpack\CRUD\app\Http\Controllers\Operations\CreateOperation {store as traitStore;}
+    use \Backpack\CRUD\app\Http\Controllers\Operations\UpdateOperation {update as traitUpdate;}
     use \Backpack\CRUD\app\Http\Controllers\Operations\DeleteOperation;
     use \Backpack\CRUD\app\Http\Controllers\Operations\ShowOperation;
 
@@ -29,19 +31,103 @@ class CommentCrudController extends CrudController
     protected function setupListOperation()
     {
         // TODO: remove setFromDb() and manually define Columns, maybe Filters
-        $this->crud->setFromDb();
+        $this->crud->setColumns([
+            [
+                'name' => 'comment',
+                'label' => 'comment',
+                'type' => 'text',
+                'limit' => 100,
+            ],
+            [
+                'name' => 'tags',
+                'label' => 'Tags(s)',
+                'type' => 'select_multiple',
+                'entity' => 'tags',
+                'attribute' => 'name',
+                'model' => Tag::class
+            ],
+        ]);
+
+        $this->crud->addFilter(
+            [
+                'type' => 'select2_multiple',
+                'name' => 'tags',
+                'label' => 'filter by tag',
+            ], function() {
+                return Tag::get()->pluck('name','id')->toArray();
+            }, function($values) {
+                foreach(json_decode($values) as $key => $value) {
+
+                    $this->crud->query = $this->crud->query->whereHas('tags', function($q) use ($value) {
+                        $q->where('tags.id', '=', $value);
+                    });
+                }
+            }
+        );
     }
 
     protected function setupCreateOperation()
     {
         $this->crud->setValidation(CommentRequest::class);
 
-        // TODO: remove setFromDb() and manually define Fields
-        $this->crud->setFromDb();
+        $this->crud->addFields([
+            [
+                'name' => 'comment',
+                'label' => 'Add your comment',
+                'type' => 'textarea',
+                'attributes' => [
+                    'rows' => 8
+                ],
+            ],
+            [
+                'name' => 'tags',
+                'label' => 'Tag the resource.',
+                'type' => 'tags',
+                'hint' => 'The dropdown contains the existing tags - start typing to filter them. If the tag you want does not exist, type it in and it will be created on save. Be generous in your tags. Similar tags will be sorted and merged later.',
+                'entity' => 'tags',
+                'attribute' => 'name',
+                'model' => Tag::class,
+                //'pivot' => true,
+                'select2_attributes' => [
+                    'tags' => true
+                ],
+            ],
+        ]);
     }
 
     protected function setupUpdateOperation()
     {
         $this->setupCreateOperation();
+    }
+
+    public function store ()
+    {
+        $tags = $this->request->spatie_tags;
+
+        $redirect_location = $this->traitStore();
+
+        $comment = Comment::find($this->data['entry']['id']);
+
+        $this->handleTags($tags, $comment);
+
+        return $redirect_location;
+    }
+
+    public function update ()
+    {
+        $tags = $this->request->spatie_tags;
+
+        $redirect_location = $this->traitUpdate();
+
+        $comment = Comment::find($this->data['entry']['id']);
+
+        $this->handleTags($tags, $comment);
+
+        return $redirect_location;
+    }
+
+    public function handleTags ($tags, $comment)
+    {
+        $comment->syncTags($tags);
     }
 }
