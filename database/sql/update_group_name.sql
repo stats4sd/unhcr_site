0f1ac9e49c87c6ec0bdc3cb1ef7ff8556ca6d49f
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-03-10 17:08:57 +0000

    modified the query to update group_name and subgroup_name into indicators table

--- database/sql/update_group_name.sql
+++ database/sql/update_group_name.sql
@@ -1,25 +1,24 @@
--- UPDATE indicators SET subgroup_name = 'No location specified' WHERE group_name = 'IDPs' ; 
--- UPDATE indicators SET subgroup_name = 'In camp' WHERE group_name = 'IDPs (in camps or settlements)' ; 
--- UPDATE indicators SET subgroup_name = 'Not in camp' WHERE group_name = 'IDPs (not in camp or settlements)' ;
--- UPDATE indicators SET subgroup_name = 'No location specified' WHERE group_name = 'Mixed' ;
--- UPDATE indicators SET subgroup_name = 'No location specified' WHERE group_name = 'Refugees' ;
--- UPDATE indicators SET subgroup_name = 'Asylum Seekers' WHERE group_name = 'Asylum Seekers' ;
+UPDATE indicators SET subgroup_name = 'No location specified' WHERE group_name = 'IDPs' ; 
+UPDATE indicators SET subgroup_name = 'In camp' WHERE group_name = 'IDPs (in camps or settlements)' ; 
+UPDATE indicators SET subgroup_name = 'Not in camp' WHERE group_name = 'IDPs (not in camp or settlements)' ;
+UPDATE indicators SET subgroup_name = 'Forcibly displaced status not specified' WHERE group_name = 'Mixed' ;
+UPDATE indicators SET subgroup_name = 'No location specified' WHERE group_name = 'Refugees' ;
+UPDATE indicators SET subgroup_name = 'Asylum Seekers' WHERE group_name = 'Asylum Seekers' ;
 
--- INSERT INTO groups (name) VALUE ('Other');
+INSERT INTO groups (name) VALUE ('Other');
 
--- INSERT INTO subgroups (name) VALUE ('IDPs');
--- INSERT INTO subgroups (name) VALUE ('in camps or settlements');
--- INSERT INTO subgroups (name) VALUE ('not in camp or settlements');
--- INSERT INTO subgroups (name) VALUE ('Mixed');
--- INSERT INTO subgroups (name) VALUE ('Refugees');
--- INSERT INTO subgroups (name) VALUE ('Asylum Seekers');
+INSERT INTO subgroups (name) VALUE ('In camp');
+INSERT INTO subgroups (name) VALUE ('Not in camp');
+INSERT INTO subgroups (name) VALUE ('No location specified');
+INSERT INTO subgroups (name) VALUE ('Forcibly displaced status not specified');
+INSERT INTO subgroups (name) VALUE ('Asylum Seekers');
 
--- UPDATE indicators SET group_name = 'IDPs' WHERE subgroup_name = 'in camps or settlements' ;
--- UPDATE indicators SET group_name = 'IDPs' WHERE subgroup_name = 'not in camp or settlements' ;
--- UPDATE indicators SET group_name = 'Other' WHERE subgroup_name = 'Mixed' ;
--- UPDATE indicators SET group_name = 'Other' WHERE subgroup_name = 'Asylum Seekers' ;
+UPDATE indicators SET group_name = 'IDPs' WHERE subgroup_name = 'In camp' ;
+UPDATE indicators SET group_name = 'IDPs' WHERE subgroup_name = 'Not in camp' ;
+UPDATE indicators SET group_name = 'Other' WHERE subgroup_name = 'Forcibly displaced status not specified' ;
+UPDATE indicators SET group_name = 'Other' WHERE subgroup_name = 'Asylum Seekers' ;
 
--- DELETE FROM groups WHERE name = 'Mixed' ;
--- DELETE FROM groups WHERE name = 'Asylum Seekers' ;
--- DELETE FROM groups WHERE name = 'IDPs (in camps or settlements)' ;
--- DELETE FROM groups WHERE name = 'IDPs (not in camp or settlements)' ;
+DELETE FROM groups WHERE name = 'Mixed' ;
+DELETE FROM groups WHERE name = 'Asylum Seekers' ;
+DELETE FROM groups WHERE name = 'IDPs (in camps or settlements)' ;
+DELETE FROM groups WHERE name = 'IDPs (not in camp or settlements)' ;
