5c04ac1d4296e74634640b5a903af3991d740c17
luciafalcinelli <44463646+luciafalcinelli@users.noreply.github.com>
2020-04-01 13:06:05 +0100

    uncomment scripts used

--- R/server.R
+++ R/server.R
@@ -44,8 +44,8 @@ server = function(input, output, session) {
     datasets <- load_dataset(input$country) 
     scripts_used <- c()
     for (id in datasets$id) {
-      print(additional_info_download(id))
-      scripts_used <- append(scripts_used, additional_info_download(id));  
+      
+      #scripts_used <- append(scripts_used, additional_info_download(id));  
       
     }
     
